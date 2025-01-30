0080eab45ac872b00abfc1fd975d5012c86e7878
Tyler Hughes <wn4493tx@go.minnstate.edu>
2025-01-30 17:27:26 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using TemperatureConverter;
+
+namespace TemperatureConverter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string userInput;
+            double doubleVal;
+            Console.WriteLine("Input temperature in farenheit: ");
+            userInput = Console.ReadLine();
+
+
+
+            doubleVal = Convert.ToDouble(userInput);
+            TempConversion converter = new TempConversion(doubleVal);
+
+            Console.WriteLine(converter.ToString());
+        }
+    }
+}
